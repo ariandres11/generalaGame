faf4536446d139faeb4f4b76354294fb575dcabd
arielandres11 <arielsebastianandres11@gmail.com>
2024-05-03 19:54:15 -0300

    Bloquear dados implementado

--- lib/components/dado.dart
+++ lib/components/dado.dart
@@ -29,34 +29,53 @@ class Juego extends StatefulWidget {
 class _JuegoState extends State<Juego> {
   var rng = new Random();
   List<int> numerosDados = [1, 1, 1, 1, 1];
+  List<bool> dadosBloq = [false,false,false,false,false];
 
   void lanzarDados() {
     setState(() {
       for (int i = 0; i < numerosDados.length; i++) {
-        numerosDados[i] = rng.nextInt(6) + 1;
+        if (!dadosBloq[i]){
+          numerosDados[i] = rng.nextInt(6) + 1;
+        }
       }
     });
   }
+
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
         Row(
-          children:
-              numerosDados.map((numero) => Dado(numDado: numero)).toList(),
+          children: numerosDados.asMap().entries.map((entry) {
+            int idx = entry.key;
+            int numero = entry.value;
+            return GestureDetector(
+              onTap: () {
+                setState(() {
+                  // Aquí inviertes el estado de bloqueo del dado correspondiente
+                  dadosBloq[idx] = !dadosBloq[idx];
+                });
+              },
+              child: Opacity(
+                opacity: dadosBloq[idx] ? 0.5 : 1, // Los dados bloqueados se muestran con menos opacidad
+                child: Dado(numDado: numero),
+              ),
+            );
+          }).toList(),
         ),
         ElevatedButton(
           onPressed: lanzarDados,
           child: Text(
-              'Lanzar dados',
-              style: TextStyle(
-                fontFamily: 'FreemanReg',
-                fontSize: 40,
-              ),
+            'Lanzar dados',
+            style: TextStyle(
+              fontFamily: 'FreemanReg',
+              fontSize: 40,
+            ),
           ),
         ),
       ],
     );
   }
+
 }
